40caf7e2c0c6b7a3cdefef9a6176462aae2f1ef5
Yash-Sajwan24 <yashsajwan12345@gmail.com>
2023-05-02 17:39:10 +0530

    useEffect to firebase

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 function App() {
 
-  const cart = useSelector(state => state.cart.isCheckout);
+  const showCart = useSelector(state => state.cart.isCheckout);
+  const cart = useSelector(state => state.cartFunction);
+
+  const url = 'https://custom-a5d73-default-rtdb.firebaseio.com/cart.json';
+
+  useEffect(() => {
+    fetch(url, {
+      method : 'PUT', 
+      body: JSON.stringify(cart),
+    })
+  }, [cart]);
 
   return (
     <Layout>
-      {cart && <Cart />}
+      {showCart && <Cart />}
       <Products />
     </Layout>
   );
